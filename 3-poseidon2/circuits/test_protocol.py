37f26596f46db8ea5fc60a28d642b333b9f7c430
stellar121 <202200460042@mail.sdu.edu.cn>
2025-07-26 23:55:00 +0800

    Delete 3-poseidon2/circuits/test_protocol.py

--- 3-poseidon2/circuits/test_protocol.py
+++
@@ -1,9 +0,0 @@
-from protocol import run_protocol
-
-# Example data
-v_list = ['u1', 'u2', 'u3']
-w_t_list = [('u2', 10), ('u3', 20), ('u4', 40)]
-
-sum_result, intersection = run_protocol(v_list, w_t_list)
-print("Intersection indexes:", intersection)
-print("Intersection sum:", sum_result)
